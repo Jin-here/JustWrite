2d628701bff9e0b8bb310d4e2ab6cd394d8e20f2
caojin <350194214@qq.com>
2015-12-14 14:48:03 +0800

    repair last commit

---
+++ 笔记/android笔记/Toolbar专题/AndroidTest/java/com/vgaw/androidtest/activity/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.vgaw.androidtest.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.vgaw.androidtest.R;
+
+/**
+ * Created by Administrator on 2015-12-14.
+ */
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seoncd);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
