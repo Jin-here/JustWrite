9792fb55f700c45fbd88adf296646d725784293b
caojin <350194214@qq.com>
2016-04-08 11:22:48 +0800

    add general

---
+++ 笔记/android笔记/QuickCoding/general/java/com/vgaw/androidtest/MainActivity.java
@@ -0,0 +1,21 @@
+package com.vgaw.androidtest;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
