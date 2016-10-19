49b2193809f68d121b9d57468f52b6e546fd8530
caojin <350194214@qq.com>
2016-10-19 16:27:28 +0800

    add something

---
+++ 笔记/android笔记/QuickCoding/utils/AnimateUtils.java
@@ -0,0 +1,16 @@
+﻿package com.vgaw.sexygirl;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by caojin on 2016/4/29.
+ */
+public class AnimateUtils {
+    private void updateWidthNHeight(View view, int width, int height){
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.width = width;
+        params.height = height;
+        view.setLayoutParams(params);
+    }
+}
